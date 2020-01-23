2add6520d0bfeb485b7b98c217a2e39f091d46cd
Kari Burt <kandreahburt@gmail.com>
2020-01-23 15:14:46 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var container = document.getElementById("container");
+var submit = document.getElementById('submit');
+function createGrid(amount) {
+  container.style.display = "grid";
+  container.style.gridTemplateColumns = `repeat(${amount}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${amount}, 1fr)`;
+  container.style.height = "500px";
+  container.style.width = "500px";
+  container.style.border = "lightgray solid 0.5px";
+  container.style.margin = "auto";
+  const area = amount * amount;
+  for (var i = 1; i <= area; i++) {
+    let content = document.createElement("div");
+    content.className = "content";
+    container.appendChild(content);
+    submit.addEventListener("click", function() {
+        content.style.backgroundColor = "white";
+    });
+      content.addEventListener("mouseover", function() {
+        content.style.backgroundColor = "grey";
+    });
+  }
+}
+function choiceAmount() {
+  var x = document.getElementById("myText").value;
+  console.log(Number(x));
+  createGrid(x);
+}
+createGrid(16);
